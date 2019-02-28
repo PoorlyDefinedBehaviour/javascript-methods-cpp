97ec0b9f461a6bb84d469ec50ddb319bae0fbb59
Bruno <brunotj2015@hotmail.com>
2019-02-28 01:32:26 -0300

    forEach now takes array and elements by reference

--- helpers/forEach.cpp
+++ helpers/forEach.cpp
@@ -10,15 +10,15 @@
  * */
 
 template <typename T, typename lambda>
-void forEach(T array, lambda func){
-  for(auto element : array){
+void forEach(T &array, lambda func){
+  for(auto &element : array){
     func(element);
   }
 }
 
 int main() {
   std::array<int, 5> array = {1,2,3,4,5};
-  forEach(array, [](auto element){
+  forEach(array, [](auto &element){
     std::cout << element << std::endl;
   });
 
