94d4f70992c38de928a45724e408d0390db7b63a
Bruno <brunotj2015@hotmail.com>
2019-03-01 01:59:36 -0300

    added concat.cpp

---
+++ helpers/concat.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+/**
+ * adds to arrays into a single new array and returns it
+ * Works for std::vector
+ * @param (array1, array2)
+ * @return std::vector
+ * */
+
+template <typename T>
+T concat(const T &array1, const T &array2)
+{
+  T newArray = array1;
+
+  for (auto element : array2)
+  {
+    newArray.push_back(element);
+  }
+
+  return newArray;
+}
+
+int main()
+{
+  std::vector<int> array1 = {1, 2, 3, 4, 5};
+  std::vector<int> array2 = {6, 7, 8, 9, 10};
+  std::vector<int> array3 = concat(array1, array2);
+
+  for (auto element : array3)
+  {
+    std::cout << element << ", ";
+  }
+}
